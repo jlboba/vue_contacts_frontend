fc8f694579ab2ada19253ca4e062cac6c6a64f9f
Jerrica Bobadilla <jerrica@Jerricas-iMac-5.home>
2020-10-19 18:21:32 -0700

    basic vue frontend complete

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,81 @@
+// HTML TEMPLATE
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Contacts</h1>
+  <ul>
+    <li 
+      v-for="contact in contacts" 
+      :key="contact.id"
+    >
+      {{contact.name}}, {{contact.age}} 
+
+      <button 
+        @click="deleteContact" 
+        :value="contact.id">
+        Delete
+      </button>
+
+      <details><summary>Edit</summary>
+        <ContactForm 
+          :contact-id="contact.id" 
+          :current-name="contact.name" 
+          :current-age="contact.age" @form-submitted="editContact"
+        />
+      </details>
+
+    </li>
+  </ul>
+  <h2>Add Contact</h2>
+  <ContactForm @form-submitted="createContact"/>
 </template>
 
+// VUE APP
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// -- dependencies 
+import axios from 'axios'
 
+// -- components 
+import ContactForm from './components/ContactForm'
+
+// -- app config
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ContactForm
+  },
+  data: function() {
+    return {
+      contacts: []
+    }
+  },
+  created: function() {
+    this.getContacts()
+  },
+  methods: {
+    getContacts: function() {
+      axios.get(process.env.VUE_APP_API_URL).then(res => {
+        this.contacts = res.data
+      })
+    },
+    deleteContact: function(e) {
+      axios.delete(`${process.env.VUE_APP_API_URL}/${e.target.value}`).then(() => {
+        this.getContacts()
+      });
+    },
+    createContact: function(contactInfo) {
+      axios.post(process.env.VUE_APP_API_URL, contactInfo).then(res => {
+        this.contacts.push(res.data)
+      })
+    }, 
+    editContact: function(updateInfo) {
+      axios.put(`${process.env.VUE_APP_API_URL}/${updateInfo.id}`, updateInfo).then(() => {
+        this.getContacts()
+      })
+    }
   }
 }
 </script>
 
+//  CSS (GLOBAL)
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
---
+++ src/components/ContactForm.vue
@@ -0,0 +1,51 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <input type="text" placeholder="name" v-model="name"/>
+    <input type="number" placeholder="age" v-model="age"/>
+    <input type="submit"/>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'ContactForm',
+  data: function() {
+    return {
+      name: '',
+      age: 0,
+      id: null
+    }
+  },
+  props: {
+    'currentName': String,
+    'currentAge': Number,
+    'contactId': Number
+  },
+  created: function() {
+    // if there's a currentName prop sent done, prefill the values
+    if(this.currentName) {
+      this.name = this.currentName
+      this.age = this.currentAge
+      this.id = this.contactId
+    }
+  },
+  methods: {
+    handleSubmit: function() {
+        this.$emit('form-submitted', {
+          name: this.name,
+          age: parseInt(this.age),
+          id: this.id
+        })
+    }
+  }
+}
+</script>
+
+//  CSS (SCOPED)
+// -- meaning it only applies to this component
+<style scoped>
+  input {
+    border-radius: 5px;
+    border: 1px solid #ccc;
+  }
+</style>
